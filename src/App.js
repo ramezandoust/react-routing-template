a32edee55f747f38f2699b1e34b2c44508ec176a
ramezandoust <abolfazl.ramezandoust@gmail.com>
2022-05-12 16:37:05 +0430

    import Element.js to App and Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { Layout } from "antd";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import MainMenu from "./components/MainMenu";
+import Element from "./components/Element";
 
 const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Layout style={{ minHeight: "100vh" }}>
-        <Header>
-          <MainMenu />
-        </Header>
+      <Router>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Header>
+            <MainMenu />
+          </Header>
 
-        <Content>Content</Content>
+          <Content>
+            <Element />
+          </Content>
 
-        <Footer>Footer</Footer>
-      </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Router>
     </div>
   );
 }

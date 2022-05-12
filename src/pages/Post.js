093e3de43696b895955479b08bcbec18c87f9eea
ramezandoust <abolfazl.ramezandoust@gmail.com>
2022-05-12 16:39:14 +0430

    load post data from localStorage and view in dom

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Card, Button } from "antd";
 
 const Post = () => {
-  return (
-    <div>Post</div>
-  )
-}
+  const [post, setPost] = useState();
 
-export default Post
+  // load post data from localStorage
+  useEffect(() => {
+    if (!localStorage.getItem("current_post")) return;
+    setPost(JSON.parse(localStorage.getItem("current_post")));
+  }, []);
+
+  return (
+    <div className="site-card-wrapper" style={{ padding: "5em" }}>
+      <Card title={post?.title} bordered={false}>
+        <p>{post?.body}</p>
+        <p>
+          <strong>ID: </strong>
+          {post?.id} - <strong>USER ID: </strong>
+          {post?.user_id}
+        </p>
+
+        <Button type="primary">
+          <a href={`/blog`}>Go Back</a>
+        </Button>
+      </Card>
+    </div>
+  );
+};
+
+export default Post;

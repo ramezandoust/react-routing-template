47e2816ef61add246b64396bdced160e57a79059
ramezandoust <abolfazl.ramezandoust@gmail.com>
2022-05-12 16:21:17 +0430

    add Antd Layout in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Layout } from "antd";
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header>Header</Header>
+
+        <Content>Content</Content>
+
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;

b7e5aab797a18cbcee063f28d04bf3432d03cd2b
ramezandoust <abolfazl.ramezandoust@gmail.com>
2022-05-12 16:28:37 +0430

    define list of page in menu and handle in antd menu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+
+const menuItems = [
+  {
+    label: <Link to="/">Home</Link>,
+    key: "",
+  },
+  {
+    label: <Link to="/blog">Blog</Link>,
+    key: "blog",
+  },
+  {
+    label: <Link to="/contact">Contact</Link>,
+    key: "contact",
+  },
+  {
+    label: <Link to="/dashboard">Dashboard</Link>,
+    key: "dashboard",
+  },
+];
 
 const MainMenu = () => {
-  return (
-    <div>MainMenu</div>
-  )
-}
+  const location = useLocation();
+  const [current, setCurrent] = useState(location.pathname.split("/")[1]);
 
-export default MainMenu
+  useEffect(() => {
+    setCurrent(location.pathname.split("/")[1]);
+  }, [location]);
+
+  const handleMenu = (e) => {
+    // setCurrent(e.key);
+  };
+  return <Menu onClick={handleMenu} selectedKeys={[current]} mode="horizontal" theme="dark" items={menuItems} />;
+};
+
+export default MainMenu;
